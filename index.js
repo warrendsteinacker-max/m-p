8bbd19ef1aea7543a7918f50008cf01a54b7253d
Warren <warrendsteinacker@gmail.com>
2026-02-10 11:26:47 -0500

    got indes.js working

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('h')
+class Name{
+    constructor(name){
+        this.name = name;
+    }
+
+    set name(name){
+        if(typeof(name) === 'string'){
+         this._name = name;}
+        }
+
+    getName(){
+        return this._name;
+    }
+
+}
+
+
+const me = new Name('John');
+
+console.log(me.getName());
+
