96f9a0379cd1dd02d22265cf780ada5ff1ad2587
Warren <warrendsteinacker@gmail.com>
2026-02-11 14:11:06 -0500

    setting up new crud command

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,30 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import './App.css'
 
-function App() {
-useEffect(() => {
-  const button1 = document.createElement('button')
-
-  button1.textContent = 'Click me post'
-  document.getElementById('root').appendChild(button1)
-
-  button1.addEventListener('click', () => {
-    const D = fetch('http://localhost:3000/posts', {method: 'GET', headers: {'content-type': 'application/json'}}).then((res)=> { return res.json()}).then((data)=> {console.log(data); alert(data)})
-  })
-
-  document.getElementById('H')
 
 
-  const e = document
-  if(e){
-    e.textContent = 'Hello World'
+
+ const Istate = {
+    name: '',
+    age: ''
   }
-}, [])
+
+  const reducer = (state, action) => {
+    switch(action.type){
+      case 'NAME':
+        return {...state, name: action.payload}
+      case 'AGE':
+        return {...state, age: action.payload}
+      default:
+        return state
+    }
+  }
+
+function App() {
+
+  const [state, dispatch] = useReducer(reducer, Istate)
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    fetch('http://localhost:3000/mposts', {method: 'POST', headers: {'content-type': 'application/json'}, body: JSON.stringify())
+    fetch('http://localhost:3000/mposts', {method: 'POST', headers: {'content-type': 'application/json'}, body: JSON.stringify(Istate)}).then((res) => {return res.json()}).then((data) => {console.log(data)}).catch((error)=> console.error(error.message))
+  }
 
 
   return (
@@ -33,13 +37,17 @@ useEffect(() => {
       </div>
       <div id='Top' > HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH </div>
 
-      <form onSubmit={}>
-        <input value={}></input>
-        <input value={}></input>
-        <button type='submit'>send to back</button>
+      <form onSubmit={handleSubmit}>
+        <input value={state.name} type='text' onChange={(e) => dispatch({type: NAME, payload: e.target.value})}></input>
+        <input value={state.age} type='text' onChange={(e) => dispatch({type: AGE, payload: e.target.value})}></input>
+        <button type='submit'>post to back</button>
       </form>
+      <button onclick={() => fetch('http://localhost:3000/posts', {method: 'GET', headers: {'content-type': 'application/json'}}).then((res)=> { return res.json()}).then((data)=> {console.log(data); alert(data)})}>Get back</button>
+      <button onClick={() => console.log(Istate)}>Istate</button>
+      <></>
     </>
   )
 }
+
 
 export default App
