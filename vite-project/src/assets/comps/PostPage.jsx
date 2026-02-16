e94e867b3d518e28b522aec47a9f68ef2bfd0887
Warren <warrendsteinacker@gmail.com>
2026-02-16 18:15:30 -0500

    nnnn

--- vite-project/src/assets/comps/PostPage.jsx
+++ vite-project/src/assets/comps/PostPage.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 const PostPage = () => {
+
+    const nav = useNavigate()
+    const [dep, setD] = useState('')
+    const [name, setN] = useState('')
+
+
+    const Mpost = async(e) => {
+        e.preventDefault()
+        try{
+            const sent = await fetch('http://localhost:3000/posts', {method: 'POST', headers: {'Content-type':'application/json'}, body: JSON.stringify({dep: dep, name: name})})
+            
+            if(sent.ok)
+                {
+                
+                console.log('SENT')
+                nav('/')
+                
+                }
+        }
+        catch(error){
+            console.error(error.message)
+            console.log('post error')
+        }
+    }
+
   return (
     <>
-    <form>
-        <input>h</input>
+    <form onSubmit={Mpost}>
+        <input type='text' placeholder='Make Post Name' value={name} onChange={(e) => setN(e.target.value)}></input>
+        <input type='text' placeholder='Make Post Depscription' value={dep} onChange={(e) => setD(e.target.value)}></input>
+        <button type='submit'>submit</button>
     </form>
     </>
   )
--- vite-project/src/assets/comps/landingP.jsx
+++ vite-project/src/assets/comps/landingP.jsx
@@ -1,15 +1,29 @@
+
 import { useEffect, useState} from "react"
 import {Link} from 'react-router-dom'
 
 const LandingP = () => {
 
-    // useEffect(() => {
+    const [D, setD] = useState([])
 
-    // }, [])
+    useEffect(() => {
+        const getS = async () => {
+        try{
+            const res = await fetch('http://localhost:3000')
+            const data = await res.json()
+            setD(data)
+        }
+        catch(error){
+            console.error(error.message + ' ' + 'could not get')
+        }}
+        getS()
+    }, [])
 
   return (
     <>
         <Link to='/addp'>make post</Link>
+        <h>posts below</h>
+        {D.map((item) => <div style={{color: 'black'}} key={item.name}><div><p>{item.dep}</p></div><div></div></div>)}
     </>
     
   )
