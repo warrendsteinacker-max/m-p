03204b9e3f75502d027d6b85763ce2733d99c8ae
Warren <warrendsteinacker@gmail.com>
2026-02-15 19:37:48 -0500

    n

--- index.js
+++ index.js
@@ -1,46 +1,82 @@
-// import express from 'express'
-// import cors from 'cors'
-// import DATA from './DATA.js'
+import express from 'express'
+import cors from 'cors'
+import DATA from './DATA.js'
+import mongoose from 'mongoose'
+const app = express()
+const PORT = 3000
 
-// const app = express()
-// const PORT = 3000
-
-
-// app.use(cors())
-// app.use(express.json())
-
-// app.get('/posts', (req, res) => {
-//   res.json(DATA)
-// })
-
-// app.post('/mposts', (req, res) => {
-//   const {name, age} = req.body
-
-//   const newD = {name, age}
-
-//   DATA.push(newD)
-
-//   res.json(DATA)}
-// )
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`)
-// })
-
-
-
-
-const obj = {
-  num1: 1,
-  num2: 2,
-  add: function() {
-    return () => {
-    console.log(this.num1 + this.num2)
-  }},
-  sub: function() {
-    return this.num1 - this.num2
+const connectDB = async() => {
+  try{
+    await mongoose.connect('mongodb+srv://warrendsteinacker_db_user:jQXpt3wf8I13h1Gp@cluster0.nzzo4zb.mongodb.net/')
+    console.log('connected')
   }
+  catch(error){
+    console.error(error.message)
+    console.log('not connected')
   }
+}
 
-  const add = obj.add()
-  add()
+connectDB()
+// jQXpt3wf8I13h1Gp
+app.use(cors())
+app.use(express.json())
+
+app.get('/posts', (req, res) => {
+  res.json(DATA)
+})
+
+app.post('/mposts', (req, res) => {
+  const {name, age} = req.body
+
+  const newD = {name, age}
+
+  DATA.push(newD)
+
+  res.json(DATA)}
+)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
+
+
+
+
+// const obj = {
+//   num1: 1,
+//   num2: 2,
+//   add: function() {
+//     return () => {
+//     console.log(this.num1 + this.num2)
+//   }},
+//   sub: function() {
+//     return this.num1 - this.num2
+//   }
+//   }
+
+//   const add = obj.add()
+//   add()
+
+
+// function timeopt() {
+//   let a = []
+//   for(let i= 0;  i<10000; ++i){
+//     a[i] = i*i
+//   }
+
+//   return function(index){
+//     console.log(a[index])
+//   }
+// }
+
+// const time = timeopt()
+
+// console.time()
+// time(9999)
+// console.timeEnd()
+
+
+
+///////////////////////wernext connection testing
+
+
---
+++ node_modules/frappe-js-sdk/lib/file/types.d.ts
@@ -0,0 +1,16 @@
+export interface FileArgs<T> {
+    /** If the file access is private then set to TRUE */
+    isPrivate?: boolean;
+    /** Folder the file exists in */
+    folder?: string;
+    /** File URL */
+    file_url?: string;
+    /** Doctype associated with the file */
+    doctype?: string;
+    /** Docname associated with the file */
+    docname?: string;
+    /** Field to be linked in the Document */
+    fieldname?: string;
+    /** Additional data to be sent along with the file */
+    otherData?: T;
+}
